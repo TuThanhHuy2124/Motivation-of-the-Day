8d15c7c0451438c83d8124adecda81a5fa6b9b7e
Thanh Huy Tu <tuthanhhuy2004@gmail.com>
2024-04-04 23:38:23 -0700

    Push first user to the database

---
+++ back/database.py
@@ -0,0 +1,17 @@
+import firebase_admin
+import json
+from datetime import datetime
+from firebase_admin import credentials, db
+
+databaseURL = "https://motivation-of-the-day-default-rtdb.firebaseio.com/"
+cred = credentials.Certificate("firebase_cred.json")
+firebase_admin.initialize_app(cred, {'databaseURL': databaseURL})
+
+ref = db.reference("/subscribers")
+
+with open("subscriber.json", "r") as file:
+    data = json.load(file)
+
+ref.push(data)
+print(ref)
+print(ref.get())
--- back/mail.py
+++ back/mail.py
@@ -1,31 +1,32 @@
 import smtplib
 from email.message import EmailMessage
 
-with open("file.txt") as f:
-    msg = EmailMessage()
-    msg.set_content(f.read())
+def send_email(receiver_email, content="file.txt"):
+    with open(content) as f:
+        msg = EmailMessage()
+        msg.set_content(f.read())
 
-sender = 'motivation.of.the.day.2124@gmail.com' 
-password = 'lewd ijxv aozh hvex'
-receiver = 'thanhhuy21112004@gmail.com'
+    sender = 'motivation.of.the.day.2124@gmail.com' 
+    password = 'lewd ijxv aozh hvex'
+    receiver = receiver_email
 
-msg['Subject'] = 'The contents of file.txt'
-msg['From'] = "Motivation Of The Day"
-msg['To'] = receiver
+    msg['Subject'] = f'The contents of {content}'
+    msg['From'] = "Motivation Of The Day"
+    msg['To'] = receiver
 
-#creating the SMTP server object
-smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
-smtp_server.ehlo() #ESMTP protocol
+    #creating the SMTP server object
+    smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
+    smtp_server.ehlo() #ESMTP protocol
 
-smtp_server.starttls() #TLS connection
-smtp_server.ehlo() #calling the ehlo() again as encryption happens on calling startttls()
+    smtp_server.starttls() #TLS connection
+    smtp_server.ehlo() #calling the ehlo() again as encryption happens on calling startttls()
 
-smtp_server.login(sender, password)
+    smtp_server.login(sender, password)
 
-# send message
-smtp_server.send_message(msg, sender, receiver)
-# print when succeed
-print('The email has been sent successfully')
+    # send message
+    smtp_server.send_message(msg, sender, receiver)
+    # print when succeed
+    print('The email has been sent successfully')
 
-#terminating the server
-smtp_server.quit()
+    #terminating the server
+    smtp_server.quit()
